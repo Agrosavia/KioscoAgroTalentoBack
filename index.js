f315b9e93f31524474166298b58652d7812ff9de
Milton Cequera <mcequera@agrosavia.co>
2025-06-06 12:34:04 -0500

    Update index.js

--- index.js
+++ index.js
@@ -1,30 +1,28 @@
-const express = require('express');
-const cors = require('cors');
-const MultimediaRouter = require('./controllers/multimedias.controller');
-const TagsRouter = require('./controllers/tags.controller');
-const EventsRouter = require('./controllers/events.controller');
-const ThemesRouter = require('./controllers/themes.controller');
-const MultimediaTypesRouter = require('./controllers/multimediaTypes.controller');
-require('dotenv').config();
+const express = require("express");
+const cors = require("cors");
+const MultimediaRouter = require("./controllers/multimedias.controller");
+const TagsRouter = require("./controllers/tags.controller");
+const EventsRouter = require("./controllers/events.controller");
+const ThemesRouter = require("./controllers/themes.controller");
+const MultimediaTypesRouter = require("./controllers/multimediaTypes.controller");
+require("dotenv").config();
 
 const port = process.env.PORT || 3001;
 const app = express();
-const path = require('path');
+const path = require("path");
 
 // Handle cors a lo maldita sea
 app.use(cors());
 
 // change all routes to routes without ApiKiosco prefix when working locally
 app.use(express.json());
-app.use("/ApiKiosco", express.static(path.join(__dirname, "public")));
-app.use("/ApiKiosco/multimedias", MultimediaRouter);
-app.use("/ApiKiosco/tags", TagsRouter);
-app.use("/ApiKiosco/events", EventsRouter);
-app.use("/ApiKiosco/themes", ThemesRouter);
-app.use("/ApiKiosco/multimedia-types", MultimediaTypesRouter);
-
+app.use(express.static(path.join(__dirname, "public")));
+app.use("/multimedias", MultimediaRouter);
+app.use("/tags", TagsRouter);
+app.use("/events", EventsRouter);
+app.use("/themes", ThemesRouter);
+app.use("/multimedia-types", MultimediaTypesRouter);
 
 app.listen(port, () => {
-    console.log('Server is running on port:' + port);
+  console.log("Server is running on port:" + port);
 });
-
